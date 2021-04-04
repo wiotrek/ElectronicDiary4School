a9fec48d2e93ad144cbabe8033a3187b6b373b6c
Kipoderax <sebastian.rajczyk@wp.pl>
2021-04-04 16:13:03 +0200

    close #3

---
+++ api/src/Controller/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return new Response(content: '<h1>Welcome</h1>');
+    }
+}

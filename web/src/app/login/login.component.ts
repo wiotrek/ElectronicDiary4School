7e38258ef26e1e8629ee8f30828234b42319d2b5
wiotrek <piotrwaszak97@gmail.com>
2021-04-04 14:32:16 +0200

    stylised login component

--- web/src/app/login/login.component.ts
+++ web/src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,11 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
   constructor(
+    private elementRef: ElementRef
   ) {
    }
 
   ngOnInit(): void {
+    // setting background color for body element
+    this.elementRef.nativeElement.ownerDocument.body.classList.add('bgForLoggingComponent');
   }
 }

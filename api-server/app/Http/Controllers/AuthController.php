21237cfe745d721d70efda5bf8d2216d235d7116
Kipoderax <sebastian.rajczyk@wp.pl>
2021-04-08 22:25:52 +0200

    close #17

---
+++ api-server/app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class AuthController extends Controller
+{
+    public function login(Request $request) {
+
+        // Get user from table by identifier
+        $user = DB::table('user')->where('identifier', '=', $request->identifier);
+
+
+        // Return bad request if user not exist
+        if (is_null($user->value('hash_pass')))
+            return new JsonResponse('nie ma takiego uzytkownika', 400);
+
+
+        // Get user password
+        $userPassword = $user->value('hash_pass');
+
+
+        // The flag indicates if password is correct
+        $isPasswordCorrect = password_verify($request->password, $userPassword);
+
+
+        // Return token for login user
+        if ($isPasswordCorrect)
+            return new JsonResponse(Str::random(60), 200);
+
+    }
+}

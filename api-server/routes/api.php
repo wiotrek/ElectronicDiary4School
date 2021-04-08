da0494228847a68c92458e49d8689ebb2384ad2d
Kipoderax <sebastian.rajczyk@wp.pl>
2021-04-08 22:25:52 +0200

    close #17

--- api-server/routes/api.php
+++ api-server/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//TODO: Seperate Routes to differents php files as userapi - UserController
+Route::get('/users', [UserController::class, 'index']);
+Route::get('/logowanie', [AuthController::class, 'login']);

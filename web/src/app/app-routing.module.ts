e4ee216382b2a32d180a3b1566b5538a6b712231
wiotrek <piotrwaszak97@gmail.com>
2021-04-09 20:47:00 +0200

    close #24

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { ClassListComponent } from './teacher/subject-list/class-list/class-list.component';
 import { SubjectListComponent } from './teacher/subject-list/subject-list.component';
 import { TeacherComponent } from './teacher/teacher.component';
 import { AuthGuard } from './_guards/auth.guard';
+import { TeacherGuard } from './_guards/teacher.guard';
 
 const routes: Routes = [
   {
@@ -12,9 +12,9 @@ const routes: Routes = [
     runGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
-      { path: 'nauczyciel', component: TeacherComponent, canActivate: [AuthGuard] },
+      { path: 'nauczyciel', component: TeacherComponent },
       { path: 'nauczyciel/:choice', component: SubjectListComponent },
-      { path: 'nauczyciel/:choice/:subject', component: ClassListComponent }
+      { path: 'nauczyciel/:choice/:subject', component: ClassListComponent, canActivate: [TeacherGuard] }
     ]
   }
 ];

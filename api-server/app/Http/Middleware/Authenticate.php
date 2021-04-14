3d4f80b6c97567a42d92fbb046219e7b72340548
wiotrek <piotrwaszak97@gmail.com>
2021-04-14 12:33:33 +0200

    add change in 26 line

--- api-server/app/Http/Middleware/Authenticate.php
+++ api-server/app/Http/Middleware/Authenticate.php
@@ -23,7 +23,7 @@ class Authenticate extends Middleware
     public function handle ( $request, Closure $next, ...$guards ) {
 
         if ($jwt = $request->cookie('jwt')){
-            $request->headers->set('Authorization', 'Bearer' . $jwt);
+            $request->headers->set('Authorization', 'Bearer ' . $jwt);
         }
         $this->authenticate($request, $guards);
 

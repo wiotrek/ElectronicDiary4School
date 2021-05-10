7d5e2e94119ea1b866f09e7c681d19833c771f84
wiotrek <piotrwaszak97@gmail.com>
2021-05-10 20:42:39 +0200

    close #90

--- web/src/app/teacher/subject-list/class-list/class/present-list/activity-list/activity-list.component.ts
+++ web/src/app/teacher/subject-list/class-list/class/present-list/activity-list/activity-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Student } from 'src/app/_models/_teacher/student';
 import { TeacherService } from 'src/app/_services/teacher.service';
 import { ToastrService } from 'ngx-toastr';
 import { StudentActivity } from 'src/app/_models/_teacher/activity/student-activity';
@@ -25,29 +24,17 @@ export class ActivityListComponent implements OnInit {
   }
 
   getStudentsList(): void {
-    this.teacherService.getStudents(this.route.snapshot.paramMap.get('class') || '')
+    this.teacherService.getStudentsActivity(this.route.snapshot.paramMap.get('class') || '')
       .subscribe(
-        (res: Student[]) => {
-          this.list = res.reduce((result: StudentActivity[], current: Student)
-          : StudentActivity[] => {
-            result.push({ student: current, isActivity: false });
-            return result; }, []);
-        }, (err: any) => console.log(err));
+        (res: StudentActivity[]) => this.list = res,
+        (err: any) => console.log(err));
   }
 
   saveList(): void {
     const subject = this.teacherService.delDashesAndUpperFirstLetter(
       this.route.snapshot.paramMap.get('subject') || '');
 
-    const a = [...this.list].reduce((total: any, current: StudentActivity): any => {
-      total.push({identifier: current.student.identifier, isActivity: current.isActivity});
-      return total;
-    }, []);
-
-    console.log(a);
-    // this.teacherService
-    //   .sendPresentList(subject, formatDate(new Date(), 'yyyy-MM-dd', 'en-Us'),
-    //   this.form.value.studentsPresent)
-    //   .subscribe(() => this.toastr.success('Obecność została zarejestrowana'));
+    this.teacherService.sendPresentList(subject, this.list)
+    .subscribe(() => this.toastr.success('Obecność została zarejestrowana'));
   }
 }

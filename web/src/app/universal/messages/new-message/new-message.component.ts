c40fa5bd39ff4221322847321b538e832b1c2253
wiotrek <piotrwaszak97@gmail.com>
2021-05-25 18:25:01 +0200

    problem with sending new message #123

--- web/src/app/universal/messages/new-message/new-message.component.ts
+++ web/src/app/universal/messages/new-message/new-message.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { AccountService } from 'src/app/_services/account.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-new-message',
@@ -7,18 +7,22 @@ import { AccountService } from 'src/app/_services/account.service';
   styleUrls: ['./new-message.component.css']
 })
 export class NewMessageComponent implements OnInit {
-  teachersObj;
-  kindOfObj;
+  @ViewChild('newMessage') newMessage!: NgForm;
   toNav = { title: 'Nowa wiadomość'};
 
-  constructor(
-    private acccountService: AccountService
-  ) {
+  teachersObj;
+  kindOfObj;
+
+  constructor() {
     this.teachersObj = this.teachers();
     this.kindOfObj = this.kindof();
   }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    console.log(this.newMessage.value);
   }
 
   teachers = () => [
@@ -44,4 +48,10 @@ export class NewMessageComponent implements OnInit {
     'Ogłoszenie', 'Uwaga', 'Ogólna wiadomość'
   ]
 
+}
+export interface NewMessage {
+  from: string;
+  to: string;
+  content: string;
+  kindof: string;
 }

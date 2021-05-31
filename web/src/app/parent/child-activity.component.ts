67e7690f87463383f8f52cad8ab78e6bd08a0781
wiotrek <piotrwaszak97@gmail.com>
2021-05-31 21:58:14 +0200

    close #132

--- web/src/app/parent/child-activity.component.ts
+++ web/src/app/parent/child-activity.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { StudentUniversal } from '../_models/_student/student-universal';
 import { Subjects } from '../_models/_student/subjects';
 import { Card } from '../_models/_universal/card';
+import { StudentService } from '../_services/student.service';
 
 @Component({
   selector: 'app-child-activity',
@@ -12,71 +13,24 @@ export class ChildActivityComponent implements OnInit {
   toUniversal = {} as StudentUniversal;
   themeColor = '#87CEFA';
 
+  constructor(private studentService: StudentService){}
+
   ngOnInit(): void {
     this.toUniversal.color = this.themeColor;
     this.toUniversal.nav = {  title: 'Frekwencja ucznia' };
 
-    this.toUniversal.header = this.fillHeader();
-    this.toUniversal.mainList = this.fillMainList();
+    this.studentService.getAvgSubjectFrequencies()
+    .subscribe((res: Card[]) => {
+      this.toUniversal.header = res
+        .reduce((total: Card[], curr: Card): Card[] => {
+          curr.color = this.themeColor;
+          curr.readonly = true;
+          curr.listViewOff = true;
+          total.push(curr);
+          return total; }, []);
+      });
+
+    this.studentService.getFrequencies()
+      .subscribe((res: Subjects[]) => this.toUniversal.mainList = res);
   }
-
-  fillHeader = (): Card[] => [
-    {
-      caption: '98%',
-      name: 'Frekwencja dziecka',
-      readonly: true,
-      listViewOff: true,
-      color: this.themeColor
-    },
-    {
-      caption: '3',
-      name: 'Pozycja na tle innych dzieci',
-      readonly: true,
-      listViewOff: true,
-      color: this.themeColor
-    }
-  ]
-
-  fillMainList = (): Subjects[] => [
-    {
-      subject: {
-        name: 'Matematyka',
-        avg: '90%',
-        position: 2,
-        countAbandoned: 3
-      },
-      days: [
-        '2021-04-21',
-        '2021-04-21',
-        '2021-04-21',
-      ]
-    },
-    {
-      subject: {
-        name: 'Matematyka',
-        avg: '90%',
-        position: 2,
-        countAbandoned: 3
-      },
-      days: [
-        '2021-04-21',
-        '2021-04-21',
-        '2021-04-21',
-      ]
-    },
-    {
-      subject: {
-        name: 'Matematyka',
-        avg: '90%',
-        position: 2,
-        countAbandoned: 3
-      },
-      days: [
-        '2021-04-21',
-        '2021-04-21',
-        '2021-04-21',
-      ]
-    }
-  ]
-
 }

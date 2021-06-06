0a93a7bc6e5fc9d02e5e06abdd0373dd9671b555
wiotrek <piotrwaszak97@gmail.com>
2021-06-06 10:42:52 +0200

    close #144

--- web/src/app/nav/nav.component.ts
+++ web/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, Renderer2 } from '@angular/core';
+import { Component } from '@angular/core';
+import { DictionaryList } from '../_models/dictionary-list';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -9,17 +10,37 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent {
   darkMode = false;
 
-  constructor(
-    private elementRef: ElementRef,
-    private renderer: Renderer2,
-    public accountService: AccountService) { }
+  darkModeListColor: DictionaryList<string> =  {
+    '--primary-blue': '#20B2AA',
+    '--primary-grey': '#708090',
+    '--line-grey': '#B0C4DE',
+    '--primary-white': '#B0C4DE',
+    '--text-black': '#E0FFFF'
+  };
+
+  lightModeListColor: DictionaryList<string> = {
+    '--primary-blue': '#2D9CDB',
+    '--primary-grey': '#F7F8FC',
+    '--line-grey': '#DFE0EB',
+    '--primary-white': '#FFFFFF',
+    '--text-black': '#000000'
+  };
+
+  constructor(public accountService: AccountService) { }
 
   logout(): void {
     this.accountService.logout();
     window.location.reload();
   }
 
+  // toggle dark mode on light mode and reverse
   darkmodeToggle = () => {
-    this.renderer.setStyle(this.elementRef.nativeElement.ownerDocument.body,'backgroundColor', 'yourchoice color');
+    this.darkMode
+    ? Object.keys(this.darkModeListColor).forEach(property => {
+        document.documentElement.style
+          .setProperty(property, this.darkModeListColor[property]);  })
+    : Object.keys(this.lightModeListColor).forEach(property => {
+      document.documentElement.style
+        .setProperty(property, this.lightModeListColor[property]); });
   }
 }

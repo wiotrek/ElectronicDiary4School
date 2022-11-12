fde4198323e3d8d70f0757855fe8faa889fa967a
wiotrek <piotrwaszak97@gmail.com>
2022-11-12 14:42:55 +0100

    #161 preapre for using dockerimage in api-server

--- makefiles/docker.mk
+++ makefiles/docker.mk
@@ -7,3 +7,13 @@ docker_web.build:
  		wiotrek/electronic-diary-web:latest -f web/Dockerfile web/.
 
 	docker push wiotrek/electronic-diary-web:latest
+
+
+# create docker image for web application and push to docker hub
+docker_api-server.build:
+	docker build \
+		--no-cache \
+		-t \
+ 		wiotrek/electronic-diary-api-server:latest -f api-server/Dockerfile api-server/.
+
+	docker push wiotrek/electronic-diary-api-server:latest
